b759e9b04e6deead2689aec94f28e87df7e024e0
Conrad Rushing <conrad.rushing@gmail.com>
2022-12-03 12:04:48 -0500

    Updating template

--- _template.rb
+++ _template.rb
@@ -8,55 +8,20 @@ require "pp"
 
 
 
-def basic_action(param_set)
-    c = 99999
-    greater_than = 0
+def basic_action(param_set, step_two = false)
+    c = 0
     for ii in param_set
-        i = ii.to_i
-        if i > c
-            greater_than += 1
-        end
-        c = i
+        c += 1
     end
-    return greater_than
+    if step_two
+        c += 1
+    end
+    c
 end
 
 
 def additional_action(param_set)
-    c = 0
-    d = 0
-
-    window_len = 2
-    sums = []
-
-    # build windowed sums: for each index of param_set, count forward to window_len, summing
-    (0..param_set.length).step(1) do |i|
-        if $DEBUG then puts i end
-        isum = 0
-        (c..c+window_len).step(1) do |jj|
-            unless param_set[jj].nil? 
-                j = param_set[jj].to_i
-                if $DEBUG then puts "\t%d" % [j] end
-                isum += j
-            end
-        end
-        if $DEBUG then puts "\t\t%d" % [isum] end
-        sums.append(isum)
-        if $DEBUG then puts "\n" end
-        c += 1
-    end
-
-#    puts sums
-
-    # find greatest
-    c = 99999999
-    for i in sums
-        if i > c
-            d += 1
-        end
-        c = i
-    end
-    return d
+    basic_action(param_set, true)
 end
 
 
@@ -103,34 +68,25 @@ end
 
 class TestSimpleNumber < Test::Unit::TestCase
     def setup
-        @test_set = [
-            199,
-            200,
-            208,
-            210,
-            200,
-            207,
-            240,
-            269,
-            260,
-            263
-        ]
+        @test_set = 'a
+b
+c'
     end
 
     def test_basic_action
         assert_equal(
-            7,
+            3,
             basic_action(
-                @test_set
+                @test_set.split("\n")
             )
         )
     end
 
     def test_additional_action
         assert_equal(
-            5,
+            4,
             additional_action(
-                @test_set
+                @test_set.split("\n")
             )
         )
     end
