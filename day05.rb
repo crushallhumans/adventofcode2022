a0fd9cc1f81f364d0038fbc646490c23305b1244
Conrad Rushing <conrad.rushing@gmail.com>
2022-12-05 10:40:52 -0500

    Day 5 solved

--- day05.rb
+++ day05.rb
@@ -8,94 +8,117 @@ require 'pp'
 require 'set'
 
 def ppd(var)
-    if $DEBUG 
-        pp var
-    end
+  if $DEBUG 
+    pp var
+  end
 end
 
 
 def basic_action(param_set, step_two = false)
-    c = 0
-    raw_stacks = []
-    instruction_params = param_set.dup
-    param_set.each do |ii|
-        raw_stack = []
-        if ii.empty? || ii == ''
-            instruction_params.shift()
-            break
-        end
-        j = 1
-        while j < ii.length do
-            raw_stack.append(ii[j])
-            j += 4
-        end
-        raw_stacks.append(raw_stack)
-        instruction_params.shift()
-        c += 1
+  c = 0
+  raw_stacks = []
+  instruction_params = param_set.dup
+  param_set.each do |ii|
+    raw_stack = []
+    if ii.empty? || ii == ''
+      instruction_params.shift()
+      break
     end
-    ppd raw_stacks
+    j = 1
+    ppd ii
+    while j < ii.length do
+      ppd [j,ii[j]]
+      raw_stack.append(ii[j])
+      j += 4
+    end
+    raw_stacks.append(raw_stack)
+    instruction_params.shift()
+    c += 1
+  end
+  ppd raw_stacks
 
-    len = raw_stacks.pop()
-    num_stacks = len.pop().to_i
+  len = raw_stacks.pop()
+  num_stacks = len.pop().to_i
 
-    stacks = Array.new(num_stacks){[]}
-    ppd stacks
+  stacks = Array.new(num_stacks){[]}
+  ppd stacks
 
+  d = 0
+  raw_stacks.reverse.each do |ii|
+    (0..num_stacks-1).each do |jj|
+      ppd jj
+      j = ii[jj]
+      if !j.nil? && j != ' '
+        stacks[jj].append(j)
+      end
+    end
+    d += 1
+  end
+
+  ppd stacks
+
+  ppd instruction_params
+
+  instruction_params.each do |ii|
+    move_amt, open_col, shut_col = ii.match(/^move (\d+) from (\d+) to (\d+)/).captures.map{|i| i.to_i}
+    open_col -= 1
+    shut_col -= 1
+    ppd [move_amt, open_col, shut_col]
     d = 0
-    raw_stacks.reverse.each do |ii|
-        (0..num_stacks-1).each do |jj|
-            ppd jj
-            j = ii[jj]
-            if j != ' '
-                stacks[jj].append(j)
-            end
-        end
-        d += 1
-    end
-
-    ppd stacks
-
-    ppd instruction_params
-
-    instruction_params.each do |ii|
-        move_amt, open_col, shut_col = ii.match(/^move (\d+) from (\d+) to (\d+)/).captures
-        ppd [move_amt, open_col, shut_col]
-    end
-
     if step_two
-        c += 1
+      #stacks[shut_col].append(stacks[open_col].pop(move_amt))
+      n = stacks[open_col].pop(move_amt)
+      if n.is_a? (String)
+        n = [n]
+      end
+      n.each {|nn| stacks[shut_col].append(nn)}
+    else
+      (0..move_amt-1).each do |j|
+        ppd "%d, %d: moving 1 from %d to %d" % [d, j, open_col,shut_col]
+        stacks[shut_col].append(stacks[open_col].pop())
+        ppd stacks
+        d += 1
+      end
     end
-    c
+  end
+
+  ppd stacks
+  c = ''
+  stacks.each do |i|
+    c += i.pop()
+  end
+
+  c
 end
 
 
 def additional_action(param_set)
-    basic_action(param_set, true)
+  basic_action(param_set, true)
 end
 
 
 
 
 def puzzle_text()
-    puts("""
+  puts("""
 """)
 end
 
 def z_actual_problem()
-    filename_script = File.basename(__FILE__)
-    puts (filename_script)
-    filename = filename_script.split(/\./)[0]
-    dirname = File.dirname(__FILE__)
-    inputname = "%s/inputs/%s_input.txt" % [dirname,filename]
-    puts inputname
-    input_set = File.readlines(inputname, chomp:true)
+  filename_script = File.basename(__FILE__)
+  puts (filename_script)
+  filename = filename_script.split(/\./)[0]
+  dirname = File.dirname(__FILE__)
+  inputname = "%s/inputs/%s_input.txt" % [dirname,filename]
+  puts inputname
+  input_set = File.readlines(inputname, chomp:true)
 
-    ret = basic_action(input_set)
-    puts ("Part 1: " + ret.to_s)
+  ret = basic_action(input_set)
+  puts ("Part 1: " + ret.to_s)
 
-    ret = additional_action(input_set)
-    puts ("Part 2: " + ret.to_s)
-    return 0
+  ret = additional_action(input_set)
+  puts ("Part 2: " + ret.to_s)
+  return 0
 end
 
 
@@ -103,22 +126,22 @@ end
 $DEBUG = false
 $PROBLEM_TEST = false
 if ARGV[0] == 'DEBUG' || ARGV[1] == 'DEBUG'
-    $DEBUG = true
+  $DEBUG = true
 end
 if ARGV[0] == 'PUZZLE'
-    puzzle_text()
-    exit()
+  puzzle_text()
+  exit()
 end
 if ARGV[0] == 'GO'
-    z_actual_problem()
-    exit()
+  z_actual_problem()
+  exit()
 end
 
 
 class TestSimpleNumber < Test::Unit::TestCase
-    def setup
-        @test_set = '    [D]    
-[N] [C]    
+  def setup
+    @test_set = '    [D]  
+[N] [C]  
 [Z] [M] [P]
  1   2   3 
 
@@ -126,25 +149,25 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2'
-    end
+  end
 
-    def test_basic_action
-        assert_equal(
-            3,
-            basic_action(
-                @test_set.split("\n")
-            )
-        )
-    end
+  def test_basic_action
+    assert_equal(
+      'CMZ',
+      basic_action(
+        @test_set.split("\n")
+      )
+    )
+  end
 
-    def test_additional_action
-        assert_equal(
-            4,
-            additional_action(
-                @test_set.split("\n")
-            )
-        )
-    end
+  def test_additional_action
+    assert_equal(
+      'MCD',
+      additional_action(
+        @test_set.split("\n")
+      )
+    )
+  end
 
 
 end
